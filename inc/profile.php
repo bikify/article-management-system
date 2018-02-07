a5276ea03799858d17c177318ad333f6f42708e9
Waqar Hussain <28485033+waqar-husain@users.noreply.github.com>
2018-02-07 18:25:12 +0500

    Update profile.php
    if a user visits profile .
    and if 
    1.he has been logged in
    1-i.he will get access to user functionalities.
    2.he has not been logged in
    2.i. he will see the detail of that user he has given user_name.

--- inc/profile.php
+++ inc/profile.php
@@ -1,12 +1,62 @@
+<head>
+<style>
+img{
+	width:120px;
+	height:150px;
+	
+	
+}
+</style>
 
-
-<head> 
-<title> My Profile  </title>
-
-
-
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/bootstrap/css/bootstrap.min.css">
+<script src="../js/jquery.min.js"></script>
+<script src="../css/bootstrap/js/bootstrap.min.js"></script>
 </head>
 
 <?php
+require_once"login.php";
+if(isLogin()){
 	include"login-header.php";
+	
+}
+
 ?>
+<?php
+require_once"login.php";
+require_once"functions.php";
+require_once"connect.inc.php";
+
+
+	$id = mysqli_real_escape_string($connection,validate(($_GET['id'])));
+    if(isset($id))
+	{
+		$sql = "SELECT * FROM `mgt_user` where user_name='$id'";
+		$result = mysqli_query($connection,$sql);
+		if(mysqli_num_rows($result))
+		{
+			if(mysqli_num_rows($result)==1){
+				
+				while($row = mysqli_fetch_assoc($result))
+				{
+					
+					$image = $row['profile_picture'];
+					echo"<img src='$image' class='img-circle'>";
+					echo "<p>USERNAME : ".$row['user_name']."</p>";
+					echo "<p>NAME : ".$row['first_name']." ".$row['last_name']."</p>";
+					echo "<title>".$row['first_name']." ".$row['last_name']."</title>";
+					echo "<p>Email : " .$row['user_email']."</p><br>";
+				}
+			}
+		}else
+		{
+			//article Dosn't Exist
+			header("location:404.php");
+		}
+    }
+
+
+
+?>
+
